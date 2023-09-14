a14529b661b991071f159e758322e7aafcbb1447
madaans <Smadaan2@rrc.ca>
2023-09-14 16:04:39 -0500

    Added preliminary files and added CalculateZ function

---
+++ module_02.py
@@ -0,0 +1,18 @@
+"""
+Description : Module 02 demonstration
+Author : SANCHI MADAAN
+Date : September 14th,2023
+Usage : To demonstrate content from Module 02.
+"""
+
+# This is a single line comment 
+
+def CalculateZ(x,y):
+    x = x 
+    y = y 
+    z = x * y 
+    return z 
+
+
+answer = CalculateZ(5,6)
+print(answer)
