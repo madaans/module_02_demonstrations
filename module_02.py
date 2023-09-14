1f42da9c39316eb5fcf56271d86c76fb7e5d3229
madaans <Smadaan2@rrc.ca>
2023-09-14 16:52:44 -0500

    added input functionality and printing variables

--- module_02.py
+++ module_02.py
@@ -15,4 +15,31 @@ def CalculateZ(x,y):
 
 
 answer = CalculateZ(5,6)
-print(answer)
+#print(answer)
+
+print("Hello","World")
+print("Hello","World", sep='_')
+print("Hello","World", sep='_', end='$')
+print("Hello","World",sep='_', end='$')
+
+name = "John"
+age = 25
+print(f"My name is{name} and I am{age} years old.")
+
+value = 3.14159
+print(f"The value is {value:.2f}.")
+
+number = 123
+print(f"The number is { number :07}.")
+
+name = "John"
+print(f"Hello, {name:>10}.")
+
+user_input=("prompt: ")
+
+name = input("Please enter your name")
+print("Name:" , name)
+
+salary = input("Please enter your salary:")
+salary = float(salary)
+print(f"Salary: ${salary:,.2f}.")
